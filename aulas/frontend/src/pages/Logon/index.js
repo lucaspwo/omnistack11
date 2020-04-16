40301544d7bc743b8f5b7a908cb6ab4758b5b01f
lucaspwo <lucaspwo@gmail.com>
2020-04-15 22:47:28 -0300

    lesson 3, 44min mark

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -11,16 +11,16 @@ export default function Logon(){
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
             </section>
-            <form>
-                <h1>Faça seu logon</h1>
-                <input placeholder="Sua ID"/>
-                <button type="submit">Entrar</button>
-                <a href="/register">
-                    <FiLogIn size={16} color="#e02041"/>
-                    Não tenho cadastro
-                </a>
-            </form>
             <img src={heroesImg} alt="Heroes"/>
         </div>
     );
