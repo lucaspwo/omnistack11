521903efe570396b2e0999ea18b78949774b8fc6
lucaspwo <lucaspwo@gmail.com>
2020-06-08 15:24:23 -0300

    lesson 4, 48m13s mark

--- aulas/mobile/src/Pages/Incidents/index.js
+++ aulas/mobile/src/Pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
@@ -16,6 +17,31 @@ export default function Incidents() {
             </View>
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+            <FlatList
+                data={[1, 2, 3, 4, 5]}
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident)}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => {}}>
+                                <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                                <Feather name="arrow-right" size={16} color="#e02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 }
